7ab34177234a73dd88f0fb5be09ea097f8b2920b
Reign Hart G. Cabasag <31159755+reignO7@users.noreply.github.com>
2020-02-07 15:50:37 +0800

    Trial

---
+++ Inventory_System/Inventory_System/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inventory_System.Models
+{
+    class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+
+        public User() { }
+        public User(string Username, string Password)
+        {
+            this.Username = Username;
+            this.Password = Password;
+        }
+    }
+}
